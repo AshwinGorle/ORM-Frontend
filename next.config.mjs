d9911d83dfefb50103cbaa443e0aca14609f847f
Bhishek-Parmar <itsbhishek@gmail.com>
2024-12-16 13:54:03 +0530

    fix the build issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,14 @@
 /** @type {import('next').NextConfig} */
-import { configDotenv } from 'dotenv';
-configDotenv();
-
-
 const nextConfig = {
-  images:{
-    domains:['images.unsplash.com'],
-    unoptimized:true
-},
-    env: {
-        NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
-      }, 
+  images: {
+    domains: ['images.unsplash.com'],
+    unoptimized: true
+  },
+  env: {
+    NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
+    NEXT_PUBLIC_LOCAL_API_URL: process.env.NEXT_PUBLIC_LOCAL_API_URL,
+    NEXT_PUBLIC_DEPLOYED_API_URL: process.env.NEXT_PUBLIC_DEPLOYED_API_URL
+  }
 };
 
 export default nextConfig;
