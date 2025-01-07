f99ca6027e72dd2f7795fa048f1257e67e005b84
Bhishek-Parmar <itsbhishek@gmail.com>
2025-01-07 21:56:09 +0530

    hide navbar on forgot password and reset-password

--- app/NavBarWrapper.js
+++ app/NavBarWrapper.js
@@ -6,9 +6,12 @@ import NavBar from "@/components/NavBar";
 export default function NavBarWrapper() {
     const pathname = usePathname();
 
-    const hideNavBarRoutes = ["/", "/login", "/signup"];
+    const hideNavBarRoutes = ["/", "/login", "/signup", "/forgot-password"];
 
-    const shouldShowNavBar = !hideNavBarRoutes.includes(pathname);
+    const shouldShowNavBar = !(
+        hideNavBarRoutes.includes(pathname) || 
+        pathname.startsWith("/reset-password/")
+    );
 
     return shouldShowNavBar ? <NavBar /> : null;
 }
